01cd9386d5c7ffec2333b56e1c0a4188381092d8
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-27 09:48:14 +0300

    drop unnecessary string

--- db.py
+++ db.py
@@ -3,8 +3,6 @@ from playhouse.migrate import *
 my_db = SqliteDatabase('buses.db')
 migrator = SqliteMigrator(my_db)
 
-title_field = IntegerField(null=True)
-
 migrate(
     migrator.add_column('stopdata', 'coordinates', IntegerField(null=True)),
 )
